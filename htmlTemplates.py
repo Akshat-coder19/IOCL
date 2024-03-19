a7fe129843ccc46b7029e852c281a86540eccaa5
Akshat <117134781+Akshat-coder19@users.noreply.github.com>
2024-03-19 15:54:29 +0530

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://www.biopetroclean.co.in/wp-content/uploads/2020/07/IOCL-Refinery.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Indian_Oil_Logo.svg/800px-Indian_Oil_Logo.svg.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
